e05ab45a37881f122acfcd99039ff461493f3020
mesarikaya <mesarikaya@gmail.com>
2019-03-08 11:27:01 +0100

    deploy for heroku

--- server.js
+++ server.js
@@ -76,6 +76,15 @@ app.get('/', function (req, res) {
 });
 */
 
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
+
 let port = 5000 || process.env.PORT;
 
 /** start server */
